12cf6c2b9ffd66a6ed19fa7b431f8d6cdacbc16e
Anton Dolgopolov <dolgopolov@soramitsu.co.jp>
2019-06-21 10:32:49 +0300

    Java support

--- src/main/kotlin/com/d3/report/walker/TestsWalker.kt
+++ src/main/kotlin/com/d3/report/walker/TestsWalker.kt
@@ -7,7 +7,8 @@ package com.d3.report.walker
 
 import java.io.File
 
-private const val TEST_FILE_EXT = ".kt"
+private const val KOTLIN_TEST_FILE_EXT = ".kt"
+private const val JAVA_TEST_FILE_EXT = ".java"
 private const val TEST_POSTFIX = "test"
 
 /*
@@ -23,7 +24,8 @@ class TestsWalker {
      */
     fun getTestFiles(rootFolder: String) = File(rootFolder).walkTopDown()
         .filter { file ->
-            file.absolutePath.endsWith(TEST_FILE_EXT) &&
+            val fileName = file.absolutePath
+            (fileName.endsWith(KOTLIN_TEST_FILE_EXT) || fileName.endsWith(JAVA_TEST_FILE_EXT)) &&
                     file.name.toLowerCase().contains(TEST_POSTFIX)
         }
         .map { file -> file.absolutePath }.toList()
